47b9d8fb56f663bc84e0ca50eaa8e2e886a9cb90
Ondiz Zarraga <ozarraga@mondragon.edu>
2015-06-08 12:06:47 +0100

    New functions implemented in main. Dummy data removed

--- nodesContact.m
+++ nodesContact.m
@@ -6,37 +6,24 @@ clc
 % 1) Leer CPRESS O CSHEAR para ver cuales son los nodos en contacto en cada
 % pieza
 
-% Vectores columna
 
-[nodosContacto1, presion1] = leerCPRESS('beam.txt');
-[nodosContacto2, presion2] = leerCPRESS('disk.txt');
+C = leerCPRESS('presion.txt');
+
+nodosContacto1 = C{1};
+nodosContacto2 = C{2};
+
+nodosContacto1 = nodosContacto1(:,1);
+nodosContacto2 = nodosContacto2(:,1);
 
 
 % 2) Para esos nodos, importar sus coordenadas
 
-% 2.1) Llamar a coordenadasInp para generar un archivo con las coordenadas
+C = coordenadasInp('beamSinAssembly.inp');
 
-%%%%%%%%%%%%%%%%%% TO DO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+coord1 = C{1};
+coord2 = C{2};
 
-% dim = 3; % Dimension del problema
-% nodos = 12136; % Numero de nodos del sistema
-% archivoLectura = 'beamOnDisk.inp';
-% coordenadas = 'coordBeamOnDisk.txt';
-
-% coordenadasInp(archivoLectura,coordenadas,dim,nodos)
-
-% 2.2) Importar el archivo de solo las coordenadas
-
-% coordenadas = load(archivoEscritura);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Por el momento usar los archivos coordBeam y coordDisk
-
-coord1 = load('coordBeam.txt');
-coord2 = load('coordDisk.txt');
-
-% 2.3) Quedarse solo con los nodos con contacto
+% Quedarse solo con los nodos con contacto
 
 coord1 = coord1(sort(nodosContacto1),:);
 coord2 = coord2(sort(nodosContacto2),:);
