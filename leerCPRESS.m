48d349dd8f49d60ee0c3893e8f1312f251c8d8e7
Ondiz Zarraga <ozarraga@mondragon.edu>
2015-06-08 11:56:39 +0100

    Directly read data from exported CPRESS

--- leerCPRESS.m
+++ leerCPRESS.m
@@ -1,16 +1,19 @@
-function [nodosContacto, presion] = leerCPRESS(archivo)
+function C = leerCPRESS(archivo)
 
-% Lee CPRESS de una superficie y devuelve los nodos en 
-% contacto para la pieza
+% Lee CPRESS de una superficie y devuelve un cell cuyos elementos son los 
+% nodos en contacto para la pieza y la presion de contacto para cada pieza
+% C{k} es una matriz de dos columnas: la primera son los nodos en contacto
+% y la segunda la presion de contacto
 
-% NOTA: de momento el archivo que se lee esta hecho a mano. Hacer un script
+texto = fileread(archivo); % Leer inp
+C = strsplit(texto,'-----------------------------------------------------------------');
+C = C(2:end); % Quitar el heading
 
-archivo = load(archivo);
-archivo = archivo(:,[3,4]);
+C = cellfun(@(x) strsplit(x,'Minimum'), C, 'UniformOutput', 0); % Coger el trozo entre *Node y *Element 
+C = cellfun(@(x) x{1}, C, 'UniformOutput', 0); % Hay que quedarse con el primer trozo de cada celda excepto de la primera
 
-archivo = archivo(archivo(:,2)~=0,:);
+C = cellfun(@str2num, C, 'UniformOutput', 0); % Convertir el string a numero
 
-archivo= unique(archivo,'rows');
-
-nodosContacto = archivo(:,1);
-presion = archivo(:,2);
+C = cellfun(@(x) x(:,[3,4]), C, 'UniformOutput', 0); % Quedarse con la tercera y cuarta columna que son los nodos en contacto y la presion de contacto
+C = cellfun(@(x) x(x(:,2)~=0,:),C, 'UniformOutput', 0); % Quitar los ceros
+C = cellfun(@(x) unique(x,'rows'),C, 'UniformOutput', 0); % Quitar los nodos repetidos
