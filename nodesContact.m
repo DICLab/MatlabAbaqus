630374e360ae02e96e2d714e6a44e9a1e66e0295
Ondiz Zarraga <ozarraga@mondragon.edu>
2015-06-12 14:58:53 +0100

    Uses shortest vector for computing contact

--- nodesContact.m
+++ nodesContact.m
@@ -1,4 +1,4 @@
-function [nodosContacto1, numeroNodo] = nodesContact(cpress, inp)
+function [nodosContacto, numeroNodo] = nodesContact(cpress, inp)
 
 % Takes CPRESS and inp from Abaqus simulation and computes which are the
 % closest nodes between two surfaces
@@ -25,13 +25,30 @@ coord2 = C{2};
 coord1 = coord1(sort(nodosContacto1)-coord1(1,1) + 1,:); % just in case it doesn't start in one
 coord2 = coord2(sort(nodosContacto2)-coord2(1,1) + 1,:); % index of nodosContacto in coord2 = value - value_first_node +1 because coord2 correlative
 
-% 3) Find closest node in surface B to node in surface A
+% 3) Find closest node in surface B to node in surface A. 
+% Use shortest vector
 
-numeroNodo = zeros(length(coord1),1); % vector that contains closest nodes to coord1
+if length(coord1) <= length(coord2)
 
-for k = 1:length(coord1)
+    nodosContacto = nodosContacto1;
+    numeroNodo = zeros(length(coord1),1); % vector that contains closest nodes to coord1
 
-    numeroNodo(k) = nodoMasCercano(coord1(k,:), coord2);
+    for k = 1:length(coord1)
+
+        numeroNodo(k) = nodoMasCercano(coord1(k,:), coord2);
+
+    end
+
+else
+    
+    nodosContacto = nodosContacto2;
+    numeroNodo = zeros(length(coord2),1); % vector that contains closest nodes to coord1
+    
+    for k = 1:length(coord1)
+
+        numeroNodo(k) = nodoMasCercano(coord2(k,:), coord1);
+
+    end
     
 end
 
