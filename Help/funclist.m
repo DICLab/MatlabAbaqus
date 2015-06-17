efe579d7a3b919ec1c2d625e68a70787b59b3e3a
Ondiz Zarraga <ozarraga@mondragon.edu>
2015-06-17 15:45:28 +0100

    Built structure for Matlab custom help

---
+++ Help/funclist.m
@@ -0,0 +1,8 @@
+%%  Functions included
+% * ScriptProfiler      
+% * leerCPRESS        
+% * coordenadasInp      
+% * nodesContact            
+% * nodoMasCercano
+% * import_matrix
+% * import_matrix3D   
